db7bf497fd0b2bfceb0bbd7898d7804aa184aa8a
Fábio de Alencar Rocha <otymus27@gmail.com>
2025-02-26 20:03:06 -0300

    Aula 12 - Uso de Try Catch

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,131 +5,133 @@ const UserService = require("../services/UserService");
 
 
 // Função para cadastrar registros
-const create = async(req, res) => {
-     //Receber os dados de um formulário e desmembrar os dados
-     const {nome, login, senha, email, foto, background} = req.body;   
+const create = async (req, res) => {
+     try {
+          //Receber os dados de um formulário e desmembrar os dados
+          const { nome, login, senha, email, foto, background } = req.body;
 
-     // Validar dados
-     if(!nome || !login || !senha || !email || !foto || !background){
-          res.status(400).send({"message":"Preencher todos os campos!"});          
+          // Validar dados
+          if (!nome || !login || !senha || !email || !foto || !background) {
+               res.status(400).send({ "message": "Preencher todos os campos!" });
+          }
+
+          // Aqui chamamos o service para cadastrar o registro no banco de dados
+          const user = await UserService.create(req.body);
+
+          if (!user) {
+               return res.status(400).send({ message: "Erro ao criar registro!" })
+          }
+
+          // Resposta para o cliente
+          res.status(201).send({
+               user: {
+                    id: user._id,
+                    nome,
+                    login,
+                    senha,
+                    email,
+                    foto,
+                    background
+               },
+               message: "Usuário criado com sucesso!"
+          });
+
+     } catch (error) {
+          res.status(500).send({ message: error.message });
      }
 
-     // Aqui chamamos o service para cadastrar o registro no banco de dados
-     const user = await UserService.create(req.body);
-
-     if(!user){
-          return res.status(400).send({message: "Erro ao criar registro!"})
-     }
-
-     //console.log(user);
-     res.status(201).send({
-          user:{
-               id:user._id,
-               nome,
-               login,
-               senha,
-               email,
-               foto,
-               background
-          },
-          message:"Usuário criado com sucesso!"
-     });
 }
 
 // Função para leitura de registros
-const listar = async(req, res) => {
-     // Variável para receber um conjunto de registros ou array
-     const users = await UserService.listar();
+const listar = async (req, res) => {
 
-     if (users.length === 0) {
-          return res.status(400).send({ message: "Nenhum registro cadastrado!" });
+     try {
+          // Variável para receber um conjunto de registros ou array
+          const users = await UserService.listar();
+
+          if (users.length === 0) {
+               return res.status(400).send({ message: "Nenhum registro cadastrado!" });
+          }
+
+          // Resposta para o cliente
+          res.status(200).send(users);
+
+     } catch (error) {
+          res.status(500).send({ message: error.message });
      }
 
-     //
-     res.status(200).send(users);
 }
 
 // Função para buscar registros por ID
-const buscarPorId = async(req, res) => {
-     // Aqui passamos o parâmetro para rota
-     const id = req.params.id;
+const buscarPorId = async (req, res) => {
+     try {
+          // Aqui passamos o parâmetro para rota
+          const id = req.params.id;
 
-     // Verificar se o parâmetro está correto - está comentado pois estamos usando middleware para isso
-     // if (!mongoose.Types.ObjectId.isValid(id)) {
-     //      return res.status(400).send({ message: "ID inválido!" });
-    // }
+          // Variável para receber o registro vindo do banco de dados, além de passarmos o parâmetro para função 
+          const user = await UserService.buscarPorId(id);
 
-     // Variável para receber o registro vindo do banco de dados, além de passarmos o parâmetro para função 
-     const user = await UserService.buscarPorId(id);
+          // Resposta para o cliente
+          res.status(200).send(user);
+     } catch (error) {
+          res.status(500).send({ message: error.message });
+     }
 
-     // Verificar se existe algum registro vindo do banco de dados - está comentado pois estamos usando middleware para isso
-     // if (!user) {
-     //      return res.status(400).send({ message: "Nenhum registro cadastrado!" });
-     // }
-
-     //
-     res.status(200).send(user);
 }
 
 // Função para excluir registros por ID
-const excluir = async(req, res) => {
-     // Aqui passamos o parâmetro para rota
-     const id = req.params.id;
+const excluir = async (req, res) => {
+     try {
+          // Aqui passamos o parâmetro para rota
+          const id = req.params.id;
 
-     // Verificar se o parâmetro está correto
-     if (!mongoose.Types.ObjectId.isValid(id)) {
-          return res.status(400).send({ message: "ID inválido!" });
+          // Verificar se o parâmetro está correto
+          if (!mongoose.Types.ObjectId.isValid(id)) {
+               return res.status(400).send({ message: "ID inválido!" });
+          }
+
+          // Variável para receber o registro vindo do banco de dados, além de passarmos o parâmetro para função 
+          const user = await UserService.excluir(id);
+
+          // Verificar se existe algum registro vindo do banco de dados
+          if (!user) {
+               return res.status(400).send({ message: "Nenhum registro cadastrado!" });
+          }
+
+          // Resposta para o cliente
+          res.status(200).send(user);
+     } catch (error) {
+          res.status(500).send({ message: error.message });
      }
 
-     // Variável para receber o registro vindo do banco de dados, além de passarmos o parâmetro para função 
-     const user = await UserService.excluir(id);
-
-     // Verificar se existe algum registro vindo do banco de dados
-     if (!user) {
-          return res.status(400).send({ message: "Nenhum registro cadastrado!" });
-     }
-
-     //
-     res.status(200).send(user);
 }
 
 // Função para cadastrar registros
-const editar = async(req, res) => {
-     //Receber os dados de um formulário e desmembrar os dados
-     const {nome, login, senha, email, foto, background} = req.body;   
+const editar = async (req, res) => {
+     try {
+          //Receber os dados de um formulário e desmembrar os dados
+          const { nome, login, senha, email, foto, background } = req.body;
 
-     // Aqui passamos o parâmetro para rota
-      const id = req.params.id;
+          // Aqui passamos o parâmetro para rota
+          const id = req.params.id;
 
-      // Verificar se o parâmetro está correto - está comentado pois estamos usando middleware para isso
-     //  if (!mongoose.Types.ObjectId.isValid(id)) {
-     //       return res.status(400).send({ message: "ID inválido!" });
-     //  }
+          // Aqui chamamos o service para buscarPorId o registro no banco de dados, passando o id 
+          const user = await UserService.buscarPorId(id);
 
-     // Validar dados - está comentado pois estamos usando middleware para isso
-     // if(!nome && !login && !senha && !email && !foto && !background){
-     //      res.status(400).send({"message":"Preencher pelo menos um campo para atualizar!"});          
-     // }
+          // Aqui chamamos o service para atualizar o registro no banco de dados, passando o id e os dados
+          await UserService.editar(id, nome, login, senha, email, foto, background);
 
-     // Aqui chamamos o service para buscarPorId o registro no banco de dados, passando o id 
-     const user = await UserService.buscarPorId(id);
+          // Resposta para o cliente
+          res.status(200).send({ message: "Registro atualizado com sucesso!" });
+     } catch (error) {
+          res.status(500).send({ message: error.message });
+     }
 
-     // Verificar se existe algum registro vindo do banco de dados - está comentado pois estamos usando middleware para isso
-   /*   if (!user) {
-          return res.status(400).send({ message: "Nenhum registro cadastrado!" });
-     } */
-
-     // Aqui chamamos o service para atualizar o registro no banco de dados, passando o id e os dados
-     await UserService.editar(id, nome, login, senha, email, foto, background);
-
-
-      // Resposta para o cliente
-      res.status(200).send({message: "Registro atualizado com sucesso!"});
 }
 
 
 
-     
 
 
-module.exports = { create, listar, buscarPorId,excluir, editar }
+
+module.exports = { create, listar, buscarPorId, excluir, editar }
--- api/middleware/global.middleware.js
+++ api/middleware/global.middleware.js
@@ -5,34 +5,43 @@ const userService = require('../services/UserService');
 
 // Função para validar o ID
 const validId = (req, res, next) => {
+    try {
+        // Receber um ID
+        const id = req.params.id;
 
-    // Receber um ID
-    const id = req.params.id;
+        // Verificar se o parâmetro está correto
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({ message: "ID inválido!" });
+        }
 
-    // Verificar se o parâmetro está correto
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).send({ message: "ID inválido!" });
+        // Função para avançar
+        next();
+    } catch (error) {
+        res.status(500).send({ message: error.message });
     }
 
-    // Função para avançar
-    next();
 }
 
 // Função para validar se registro existe
 const validUser = async (req, res, next) => {
-    // Receber um ID
-    const id = req.params.id;
+    try {
+        // Receber um ID
+        const id = req.params.id;
 
-    // Verificar se o parâmetro está correto
-    const user = await userService.buscarPorId(id);
+        // Verificar se o parâmetro está correto
+        const user = await userService.buscarPorId(id);
 
-    // Verificar se existe algum registro vindo do banco de dados
-    if (!user) {
-        return res.status(400).send({ message: "Nenhum registro cadastrado!" });
+        // Verificar se existe algum registro vindo do banco de dados
+        if (!user) {
+            return res.status(400).send({ message: "Nenhum registro cadastrado!" });
+        }
+
+        // Função para avançar
+        next();
+    } catch (error) {
+        res.status(500).send({ message: error.message });
     }
 
-    // Função para avançar
-    next();
 
 }
 
