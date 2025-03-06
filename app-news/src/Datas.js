8a29512aa03dde8a55b7f0bf0408243208df9e84
Fábio de Alencar Rocha <otymus27@gmail.com>
2025-03-06 17:35:34 -0300

    2º Commit - create data mock ou dados mocados

---
+++ app-news/src/Datas.js
@@ -0,0 +1,43 @@
+export const noticias = [
+
+     {
+          titulo: "Noticia 1",
+          texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          banner: "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+          likes: 2,
+          comments: 4
+     },
+
+     {
+          titulo: "Noticia 1",
+          texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          banner: "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+          likes: 2,
+          comments: 4
+     },
+
+     {
+          titulo: "Noticia 1",
+          texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          banner: "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+          likes: 2,
+          comments: 4
+     },
+
+     {
+          titulo: "Noticia 1",
+          texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          banner: "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+          likes: 2,
+          comments: 4
+     },
+
+     {
+          titulo: "Noticia 1",
+          texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          banner: "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+          likes: 2,
+          comments: 4
+     },
+
+]
